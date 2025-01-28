26ce549823071ec8bece608caafa9c65cbd4da71
Kraknes <erling.heimstad.willassen@gmail.com>
2025-01-28 11:10:58 +0100

    ny struct

--- pointers.c
+++ pointers.c
@@ -1,26 +1,66 @@
 #include <stdio.h>
+#include <stdlib.h>
 // test
 
-void main(){
+struct node{
+    int a;
+    int b;
+};
+
+struct node *create_node(){
+    struct node *hey = malloc(sizeof(struct node));
+    hey->a = 5;
+    hey->b = 10;
+    return hey;
+}
+
+typedef struct Noder{
+    int a;
+    int b;
+} node_test;
+
+
+int main(){
     int a = 5;
     int b = 6;
     int * c = &a;
     int * ptr;
     ptr = &b; 
 
-    printf("\n%d", a);
-    printf("\n%d", b);
-    printf("\n%p", c);
-    printf("\n%p", &a);
-    printf("\n%p", ptr);
-    printf("\n%p", &b);
-    printf("\n%d", *ptr);
+
+
+    printf("\n%d verdi av node->a", a);
+    printf("\n%d verdi av node->b", b);
+    printf("\n%p verdi av pointer c til a", c);
+    printf("\n%p verdi av pointer node->a", &a );
+    printf("\n%p verdi av pointer ptr til b", ptr);
+    printf("\n%p verdi av pointer node->b", &b);
+    printf("\n%d verdi av verdien til *ptr som pointer til b", *ptr);
 
     *ptr += 10;
-    printf("\n%d", *ptr);
-    printf("\n");
+    printf("\n%d verdien til *ptr som er lagt til 10 som er lagt til b", *ptr);
 
-    // int* c = (int* 10);
-    // printf("\n%d", a);
+    int * ptr_2;
+    ptr_2 = &b;
+
+    printf("\n%d verdien til ptr_2 som pointer til b (et resultat av at *ptr ble endret på fra sist linje)", *ptr_2);
+    
+
+
+    struct node * test = create_node();
+    printf("\n%d er verdien til node->a",test->a);
+    
+    node_test hey;
+    hey.a = 6;
+    hey.b = 11;
+
+    printf("\n%d verdien til typedef node struct hey.a", hey.a);
+    printf( "\n%i verdien til typedef node struct hey.b", hey.b);
+
+
+
+
+    printf("\n");
+    return 0;
 
 }
